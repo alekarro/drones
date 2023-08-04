4c602b643467bf71e7882da9695154cdd2d322ab
alerro <sapid777>
2023-08-04 14:52:42 +0200

    removed junk code

--- drones-data/src/main/java/org/musala/drones/data/model/ModelEnum.java
+++
@@ -1,6 +0,0 @@
-package org.musala.drones.data.model;
-
-public enum ModelEnum {
-    //Lightweight, Middleweight, Cruiserweight, Heavyweight
-    LIGHTWEIGHT, MIDDLEWEIGHT, CRUISERWEIGHT, HEAVYWEIGHT
-}
--- drones-data/src/main/java/org/musala/drones/data/model/StateEnum.java
+++
@@ -1,5 +0,0 @@
-package org.musala.drones.data.model;
-
-public enum StateEnum {
-    IDLE, LOADING, LOADED, DELIVERING, DELIVERED, RETURNING
-}
